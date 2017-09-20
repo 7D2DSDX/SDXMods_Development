5e10ed10ecd851a9f3d99f916b05c4ca4457b9bd
SphereII <support@7d2dlauncher.org>
2017-09-19 22:12:28 -0300

    adding test mod

---
+++ HalDarkRun/Scripts/EntityZombieLight.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityZombieSDX: EntityZombie
+    {
+
+    // Update the Approach speed, and add a randomized speed to it
+    public override float GetApproachSpeed()
+    {
+        if (GamePrefs.GetInt(EnumGamePrefs.ZombiesRun) == 1)
+        {
+            return this.speedApproach * this.Stats.SpeedModifier.Value;
+        }
+        else
+        {
+            if (this.world.IsDark())
+                return this.speedApproachNight * UnityEngine.Random.Range( 0.2f,1.2f);
+            else
+                return this.speedApproach * UnityEngine.Random.Range(0.2f, 1.2f);
+        }
+    }
+
+    // Randomize the Walk types.
+    public override int GetWalkType()
+    {
+        int WalkType = base.GetWalkType();
+        if (WalkType == 4)
+            return WalkType;
+        
+        return UnityEngine.Random.Range(1, 8);
+    }
+}
+
