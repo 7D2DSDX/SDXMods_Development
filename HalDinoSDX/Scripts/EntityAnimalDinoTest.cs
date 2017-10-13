e9c0be65201391f55d49316e243ace8015d8c5b2
SphereII <support@7d2dlauncher.org>
2017-10-13 17:38:58 -0300

    adding dino demos

---
+++ HalDinoSDX/Scripts/EntityAnimalDinoTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+public class EntityAnimalDinoTest : EntityAnimal
+{
+    public EntityAnimalDinoTest() : base()
+    {
+
+    }
+    
+
+    protected override void Awake()
+    {
+        base.Awake();
+        
+    }
+
+    public override int DamageEntity(DamageSource _damageSource, int _strength, bool _criticalHit, float impulseScale)
+    {
+        return base.DamageEntity(_damageSource, _strength, _criticalHit, impulseScale);
+    }
+    public override Vector3 GetMapIconScale()
+    {
+        return new Vector3(0.45f, 0.45f, 1f);
+    }
+}
