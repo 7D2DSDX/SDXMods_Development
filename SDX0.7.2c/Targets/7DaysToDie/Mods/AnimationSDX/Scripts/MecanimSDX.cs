25b44b6ced38614126999be635acbc2194a8172d
SphereII <kilpatrick.jeff@gmail.com>
2018-02-01 09:09:46 -0400

    fixing attacks for the AttackHash

--- SDX0.7.2c/Targets/7DaysToDie/Mods/AnimationSDX/Scripts/MecanimSDX.cs
+++ SDX0.7.2c/Targets/7DaysToDie/Mods/AnimationSDX/Scripts/MecanimSDX.cs
@@ -114,7 +114,7 @@ class MecanimSDX : MonoBehaviour, IAvatarController
             this.rightHandItemTransform = FindTransform(this.bipedTransform, this.bipedTransform, RightHand);
         }
 
-        this.AttackHash = GenerateLists(entityClass, "AttackAnimations", this.AttackStrings);
+        this.AttackHash = GenerateLists(entityClass, "Attacks", this.AttackStrings);
 
 
         // The following will read our Index values from the XML to determine the maximum attack animations.
